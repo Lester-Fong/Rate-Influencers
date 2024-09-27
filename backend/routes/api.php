7298d9ad5679c70e5f78a5cfa9ca60603bbdca0c
Lester Fong <lester.fong@switchconnect.com.au>
2024-09-27 14:31:06 +0800

    Add login route for AdministratorController

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,9 +1,11 @@
 <?php
-
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\InfluencerController;
 use App\Http\Controllers\CommentController;
+use App\Http\Controllers\AdministratorController;
+
+// Login Admin
+Route::post("/login", [AdministratorController::class, "login"]);
 
 // Dislay all Influencers
 Route::get("/", [InfluencerController::class, "index"]);
@@ -11,6 +13,7 @@ Route::get("/", [InfluencerController::class, "index"]);
 Route::get("/{slug}", [InfluencerController::class, "show"]);
 // Store Comment by Slug
 Route::post("/{slug}", [CommentController::class, "store"]);
+
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
