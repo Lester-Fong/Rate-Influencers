c222bfdcd0c79051e78ce31a2be9813ecb435987
Lester Fong <lester.fong@switchconnect.com.au>
2024-10-01 22:10:51 +0800

    sanctum configurations

--- backend/app/Http/Controllers/AdministratorController.php
+++ backend/app/Http/Controllers/AdministratorController.php
@@ -2,18 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Administrator;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Log;
 
 class AdministratorController extends Controller
 {
-    public function login(Request $request): void {
+    public function login(Request $request) {
         $data = $request->validate(rules: [
             "email" => "required|email",
             "password" => "required"
         ]);
 
-        Log::debug(message: print_r(value: $data, return: true));
-        dd(vars: 'test');
+        $admin = Administrator::where("email", $data["email"])->first();
+        if (!isset($admin) || !Hash::check($data["password"], $admin->password)) {
+            return response()->json(["error" => true, "message" => "Invalid credentials"], 401);
+        }
+
+        $token = $admin->createToken($admin->id . "admin")->plainTextToken;
+
+        return response()->json(["error" => false, "token" => $token, "admin" => $admin]);
     }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\CommentController;
 use App\Http\Controllers\AdministratorController;
 
 // Login Admin
+
 Route::post("/login", [AdministratorController::class, "login"]);
 
 // Dislay all Influencers
@@ -15,6 +16,5 @@ Route::get("/{slug}", [InfluencerController::class, "show"]);
 Route::post("/{slug}", [CommentController::class, "store"]);
 
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
+// Route::middleware("auth:sanctum")->group(function() {
+// });
