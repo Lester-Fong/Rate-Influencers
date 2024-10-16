517027a2186f276bb4d8a759aef920466e3bbe68
Lester Fong <lester.fong@switchconnect.com.au>
2024-10-16 19:32:17 +0800

    Added sidebar

---
+++ frontend/src/components/Sidebar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="flex items-start justify-between flex-col w-full">
+    <div class="mt-5 mb-10 text-center w-full font-bold text-2xl">
+      <h1 class="text-teal-300 ir-main_title text-2xl tracking-wider">Rate Influencers</h1>
+      <hr class="mt-4 opacity-25 mx-4" />
+    </div>
+    <ul class="px-4 w-full">
+      <li class="pb-3 cursor-pointer text-gray-400 font-medium hover:text-sky-400">
+        <router-link :to="{ name: 'dashboard' }">Dashboard</router-link>
+      </li>
+      <li class="pb-3 cursor-pointer text-gray-400 font-medium hover:text-sky-400">
+        <router-link :to="{ name: 'dashboard' }">Influencers</router-link>
+      </li>
+      <li class="pb-3 cursor-pointer text-gray-400 font-medium hover:text-sky-400">
+        <router-link :to="{ name: 'dashboard' }">Dashboard</router-link>
+      </li>
+    </ul>
+  </div>
+</template>
