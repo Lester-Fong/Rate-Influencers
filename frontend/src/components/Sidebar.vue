9eeb1aa34dc18ed8816114561db6f3c63d04b4f2
Lester Fong <lester.fong@switchconnect.com.au>
2024-11-12 18:55:43 +0800

    Update Sidebar component to ensure full height layout

--- frontend/src/components/Sidebar.vue
+++ frontend/src/components/Sidebar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex items-start justify-between flex-col w-full">
+  <div class="flex items-start justify-between flex-col w-full h-full">
     <div class="mt-5 mb-10 text-center w-full font-bold text-2xl">
       <h1 class="text-gray-400 ir-main_title text-2xl tracking-wider">Rate Influencers</h1>
       <hr class="mt-4 opacity-25 mx-4" />
